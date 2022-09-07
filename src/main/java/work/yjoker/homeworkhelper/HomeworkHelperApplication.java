37458b6281334e247368bdbfeb514d865fa86b74
YunJia <sanchuanguli@163.com>
2022-09-07 11:50:33 +0800

    init

---
+++ src/main/java/work/yjoker/homeworkhelper/HomeworkHelperApplication.java
@@ -0,0 +1,13 @@
+package work.yjoker.homeworkhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/work/yjoker/homeworkhelper/HomeworkHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package work.yjoker.homeworkhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
